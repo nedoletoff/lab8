2e90fa471905514b006c3a154384e2e580b553f7
nedoletoff <nedoletoff@yandex.ru>
2021-02-27 12:10:44 +0300

    Добавлена реализация доп задания

--- a.cpp
+++ a.cpp
@@ -3,18 +3,23 @@
 #include <stdlib.h>
 #define N 5
 
-int type_int()
+int type_int(int check)
 {
-	int n = 0;
-	while(n < N)
+	int n = 0, r = 1;
+	while (r)
 	{
-		printf("Number should be greater then %d\n", N); 
+		n = 0;
+		if (!check)
+			n = N;
+		printf("Number should be greater then %d\n", n); 
 		printf("Type int number: ");
 		while (scanf("%d", &n) != 1 || getchar() != '\n') 
 		{
 			printf("Error, try again\n");
 			while (getchar() != '\n');
 		}
+		if (check && n > 0 || n > N)
+			r = 0;
 	}
 	return n;
 }
@@ -35,6 +40,7 @@ void pull_arr(double* arr, int size)
 {
 	for (int i = 0; i < size; ++i)
 		arr[i] = type_double();
+	putchar('\n');
 }
 
 void update_abs_arr(double* abs_arr, double* arr, int size)
@@ -61,4 +67,25 @@ void print_arr(double* abs_arr, double* arr, int size)
 	for (int i = 0; i < N; ++i)
 		printf("\t%lf", arr[(num + i) % size]);
 	putchar('\n');
+}
+
+int get_index(double* arr, int size, int gap)
+{
+	double max;
+	double temp = 0;
+	int index = -1;
+	for (int i = 0; i < size; ++i)
+	{
+		temp = 0;
+		for (int j = 0; j < gap; ++j)
+			temp += arr[(i + j) % size];
+		temp /= gap;
+		if (index == -1 || temp > max)
+		{
+			max = temp;
+			index = i;
+		}
+	}
+	printf("Ans = %d\n", index);
+	return index;
 }
--- a.hpp
+++ a.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-int type_int();
+int type_int(int);
 void pull_arr(double*, int);
 void update_abs_arr(double*, double*, int);
 void print_arr(double*, double*, int);
+int get_index(double*, int, int);
