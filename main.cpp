73eeacf2d7b90ab79c6dc4f008da0c689811441b
nedoletoff <nedoletoff@yandex.ru>
2021-03-08 15:28:19 +0300

    Updated

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ int main()
 {
 	int n = 0, gap = 0;
 	double* arr = NULL;
-	double* abs_arr = NULL;
+//	double* abs_arr = NULL;
 	n = type_int(0);
 	arr = (double*) calloc(n, sizeof(double));
-	abs_arr = (double*) calloc(n, sizeof(double));
+//	abs_arr = (double*) calloc(n, sizeof(double));
 	pull_arr(arr, n);
-	update_abs_arr(abs_arr, arr, n);
-	print_arr(abs_arr, arr, n);
+//	update_abs_arr(abs_arr, arr, n);
+//	print_arr(abs_arr, arr, n);
 	gap = type_int(1);
 	get_index(arr, n, gap);
 	return 0;
